efecbe57929201c4bc920dc37630a42d99c44c2d
apriliyanto dwi saputra <aprildwi999@gmail.com>
2025-09-24 15:35:17 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="bg-neutral-950 text-white z-index-10 fixed w-full top-0">
+        <div class="flex justify-between items-center p-2 max-w-7xl mx-auto">
+            <div class="flex items-center space-x-4">
+              <img src="@/assets/MyLogo.png" alt="MyLogo" class="w-12 h-12 rounded-full"/>
+              <h1 class="text-lg font-extrabold">aprll_028</h1>
+            </div>
+            <nav class="flex space-x-6 text-sm font-semibold">
+              <router-link to="/">Home</router-link>
+              <router-link to="/">Project</router-link>
+              <router-link to="/">Sertifikat</router-link>
+              <router-link to="/">Blog</router-link>
+              <router-link to="/">Contact Me</router-link>
+            </nav>
+        </div>
+    </div>
+  </header>
+  <div class="content mt-16 bg-zinc-900 text-white min-h-screen">
+    <RouterView />
+  </div>
+  <footer>
+    <div class="bg-neutral-950 text-neutral-400 p-2 text-center">
+      <p class="text-sm">&copy; 2024 Aprliyanto Dwi Saputra. All rights reserved.</p>
+      <p class="text-sm">Build & Design by Me</p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,105 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="bg-zinc-900 text-white min-h-screen p-8 pt-20">
+    <div class="hero max-w-4xl mx-50 grid grid-cols-2 items-center gap-8 mt-5">
+      <div class="text">
+        <span class="font-bold text-stone-500">ABOUT ME</span>
+        <h1 class="text-3xl font-bold">I Design with Logic, Code with Creativity.</h1>
+        <p class="mt-4 leading-relaxed">
+          I am an Informatics student with a strong dedication and enthusiasm 
+          for exploring knowledge and innovation in technology.
+        </p>
+        <div class="flex space-x-4 mt-10">
+          <i class="fa-solid fa-envelope fa-xl"></i>
+          <i class="fa-brands fa-whatsapp fa-xl"></i>
+          <i class="fa-brands fa-instagram fa-xl"></i>
+          <i class="fa-brands fa-github fa-xl"></i>
+        </div>
+      </div>
+      <div class="img">
+        <img 
+          src="@/assets/MyFoto.png" 
+          alt="Home Image" 
+          class="object-cover object-bottom-right shadow-lg h-80 w-80 mx-40"
+        />
+      </div>
+    </div>
+
+    <div class="skill w-full pt-40">
+      <h2 class="text-3xl font-bold text-center mb-10">My Skills</h2>
+      <div class="grid grid-cols-4 gap-5">
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">HTML & </h3>
+          <p>Proficient in structuring web content.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Adobe Ilustrator</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">PHP</h3>
+          <p>Skilled in styling and layout design.</p>
+          <img src="" alt="">
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Photoshop</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Javascript</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">MySql</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">After Effect</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Bootstrap</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Tailwin</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">JQuery</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Laravel</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Vue</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">React</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Git</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Firebase</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+        <div class="skill-item bg-zinc-800 p-4 rounded-lg text-center">
+          <h3 class="font-semibold mb-2">Figma</h3>
+          <p>Skilled in styling and layout design.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
